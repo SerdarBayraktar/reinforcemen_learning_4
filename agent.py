2cb4f51a5ce426e1aee1a7c548ae47ac1098151b
Serdar Bayraktar <serdarbayraktarmath@gmail.com>
2024-05-24 18:40:35 +0200

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,21 +1,24 @@
 import numpy as np
 class BoltzmanPolicy:
 
-    def __init__(self, n_actions=10):
+    def __init__(self, n_actions=10,t=1.0, alpha=0.1):
         self.n_actions = n_actions
         self.Q = np.zeros(n_actions)
         self.n = np.zeros(n_actions)
-
+        self.t = t
+        self.alpha = alpha
         pass
 
     def select_action(self, epsilon):
         #boltzman
-        pass
+        exp_Q = np.exp(self.Q / self.t)
+        probabilities = exp_Q / np.sum(exp_Q)
+        action = np.random.choice(self.n_actions, p=probabilities)
+        return action
 
     def update(self, a, r):
         self.n[a] += 1
-        self.Q[a] += (r - self.Q[a]) / self.n[a]
-        pass
+        self.Q[a] += self.alpha * (r - self.Q[a])
 
 
 class ThompsonPolicy:
